db1eeb353cdc33bcc4cc57a84dfa6f9f9146afc9
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2024-12-26 06:50:05 +0000

    string ops & conditions

---
+++ 04_Language_Components/a-Relational-Operations/a-Relational-Operations.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	usd := 76
+	cand := 50
+
+	fmt.Println("usd < cand  =", usd < cand)
+	fmt.Println("usd <= cand =", usd <= cand)
+	fmt.Println("usd > cand  =", usd > cand)
+	fmt.Println("usd >= cand =", usd >= cand)
+	fmt.Println("usd == cand =", usd == cand)
+
+	fmt.Println("usd != cand =", usd != cand)
+	// fmt.Println("usd <> cand =", usd <> cand)
+	// syntax error: unexpected >, expecting expression
+
+	fmt.Println(12 < 34) // true
+
+	// fmt.Println(12 < 34 < 45)
+	// invalid operation: 12 < 34 < 45 (mismatched types untyped bool and untyped int)
+
+	fmt.Println(12 < 34)       // true
+	fmt.Println(1.2 < 3.4)     // true
+	fmt.Println("1.2" < "3.4") // true
+	fmt.Println('1' < '3')     // true
+	fmt.Println('1' < 3)       // false
+
+	// string comparision
+	fmt.Println("apple" < "apparao") // false
+
+	
+	fmt.Println(4 == 4.0) // true
+	// var (
+	// 	num3 int     = 4
+	// 	num4 float32 = 4.0
+	// )
+	// fmt.Println("num3 == num4:", num3 == num4)
+	// invalid operation: num3 == num4 (mismatched types int and float32)
+}
+
+// assignment - try for all other builtin data types
---
+++ e_Conditions/a-if-else/f-office-timings.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+/*
+	OFFICE TIMINGS
+		Monday 		9 AM to 6 PM
+		Tuesday		9 AM to 6 PM
+		Wednesday	9 AM to 6 PM
+		Thursday	9 AM to 6 PM
+		Friday 		9 AM to 6 PM
+		Saturday	9 AM to 1 PM
+		Sunday 		HOLIDAY
+*/
+
+func main() {
+	fmt.Print("Enter day of the week:")
+
+	var weekOfDay string
+	fmt.Scanf("%s", &weekOfDay) // will pick only the characters
+
+	// converting to lower case
+	weekOfDay = strings.ToLower(weekOfDay)
+
+	if weekOfDay == "monday" { // || weekOfDay == "Monday"
+		println("Timings: 9 AM to 6 PM ")
+	} else if weekOfDay == "tuesday" {
+		println("Timings: 9 AM to 6 PM ")
+	} else if weekOfDay == "wednesday" {
+		println("Timings: 9 AM to 6 PM ")
+	} else if weekOfDay == "thursday" {
+		println("Timings: 9 AM to 6 PM ")
+	} else if weekOfDay == "friday" {
+		println("Timings: 9 AM to 6 PM ")
+	} else if weekOfDay == "saturday" {
+		println("Timings: 9 AM to 1 PM ")
+	} else if weekOfDay == "sunday" {
+		println("----HOLIDAY ----------")
+	} else {
+		println("INVALID ENTRY")
+	}
+
+}
+
+// NOTE: else block  is not mandatory
